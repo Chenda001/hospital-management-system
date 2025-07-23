8cd41319d9cdde2ba1ff0ed0d49f87e9726a3308
benjamin <titusben64@gmail.com>
2025-07-23 14:41:57 +0300

    commit changes

--- pwani_hospital/admin.py
+++ pwani_hospital/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import HomeContent, AboutSection, Service, ContactMessage, Footer
+
+admin.site.register(HomeContent)
+admin.site.register(AboutSection)
+admin.site.register(Service)
+admin.site.register(ContactMessage)
+admin.site.register(Footer)
+
+
+
 # Register your models here.
--- pwani_hospital/views.py
+++ pwani_hospital/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
+def return_base(request):
+    return render(request, 'pwani_hospital/base.html')
+
 def index(request):
-    return render(request, 'index.html')
+   return render(request, 'pwani_hospital/index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'pwani_hospital/about.html')
 
 def services(request):
-    return render(request, 'services.html')
+    return render(request, 'pwani_hospital/services.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'pwani_hospital/contact.html')
+def Footer(request):
+    footer = Footer.objects.first()
+    return render(request, 'pwani_hospital/base.html', {'footer': footer})
